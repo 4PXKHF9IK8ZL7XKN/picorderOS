b3f9846db3be5a2487ce9db2f2fdd9989eeeabf5
4PXKHF9IK8ZL7XKN <95d4938ovw6vh9mw62ik@mailbox.org>
2024-06-29 07:14:35 +0200

    changed the test to play a sound and listening to touch

--- receive_logs_topic.py
+++ receive_logs_topic.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import pika
 import sys
+from pygame import mixer
+
+mixer.init()
+alert=mixer.Sound('assets/beep.wav')
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
@@ -12,14 +16,14 @@ result = channel.queue_declare('', exclusive=True)
 queue_name = result.method.queue
 
 channel.queue_bind(
-    exchange='sensor_data', queue='', routing_key='generators')
+    exchange='sensor_data', queue='', routing_key='touch')
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
 
 def callback(ch, method, properties, body):
     print(f" [x] {method.routing_key}:{body}")
-
+    alert.play()
 
 channel.basic_consume(queue='',on_message_callback=callback, auto_ack=True)
 
