64e91823dca9748f73b174d06aa5f01be62d4212
4PXKHF9IK8ZL7XKN <95d4938ovw6vh9mw62ik@mailbox.org>
2024-08-18 16:06:53 +0200

    added a worker thread for publishing keypresses

---
+++ picos_pika_worker.py
@@ -0,0 +1,56 @@
+#!/bin/python
+import time
+import os
+import struct
+import sys
+import pika
+
+from objects import *
+
+DEBUG = False
+
+if configure.rabbitmq_remote:
+	credentials = pika.PlainCredentials(configure.rabbitmq_user,configure.rabbitmq_password)
+	connection = pika.BlockingConnection(pika.ConnectionParameters(configure.rabbitmq_address,configure.rabbitmq_port,configure.rabbitmq_vhost,credentials))
+	channel = connection.channel()
+else:
+	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+	channel = connection.channel()
+
+def declare_channel():
+	# Setup Channels for Sensors
+	channel.exchange_declare(exchange='sensor_data', exchange_type='topic')
+	channel.queue_declare(queue='sensor_metadata')
+	# cleanup entreys , we could have multible index
+	channel.queue_purge(queue='sensor_metadata')
+
+def publish_worker(IN_routing_key,data):
+	stack = 'sensor_data'
+	if IN_routing_key == 'sensor_metadata':
+		stack = ''
+
+	routing_key = str(IN_routing_key)
+	message = str(data)
+	time_unix = time.time()
+	try:
+		if message is not None:
+			channel.basic_publish(exchange=stack, routing_key=routing_key, body=message)
+		else:
+			print("Is that a buffer underflow?")
+	except Exception as e:
+		print("An error occurred:",e)
+		try:
+			raise Exception('Terminating')
+		finally:
+			os._exit(1)
+	if DEBUG:
+		print(f" {time_unix} [x] Sent {stack} {routing_key}:{message}")
+
+def disconnect():
+    connection.close()
+
+def main_pika_worker(var,var2):
+	declare_channel()
+	publish_worker(var,var2)
+	exit()
+
