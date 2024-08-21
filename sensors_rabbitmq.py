e5179f3b7b2db03dab4d2f4b66bfdd0c5c5daa6c
4PXKHF9IK8ZL7XKN <95d4938ovw6vh9mw62ik@mailbox.org>
2024-08-21 11:59:15 +0200

    updatet gps with more fully parsing

--- sensors_rabbitmq.py
+++ sensors_rabbitmq.py
@@ -9,6 +9,7 @@ import threading
 import pika
 import signal
 import serial
+import datetime
 import picos_pika_worker as publisher_worker
 from pynmeagps import NMEAReader
 import RPi.GPIO as GPIO
@@ -222,36 +223,153 @@ class Job(threading.Thread):
             while not self.stopped.wait(self.interval.total_seconds()):
                 self.execute(*self.args, **self.kwargs)
 
-
-def GPS_function():
-		gps_update = {"lat" : None, "lon" : None, "speed" : 0.00,"altitude":0.00, "track" : 0.00, "sats":0}
-
-		stream = serial.Serial(port, baud, timeout=3)
-		nmr = NMEAReader(stream)
-		(raw_data, parsed_data) = nmr.read()
-
-		if hasattr(parsed_data, "lat"):
-
-			if parsed_data.lat != '':
-				gps_update["lat"] = float(parsed_data.lat)
-
-			if parsed_data.lon != '':
-				gps_update["lon"] = float(parsed_data.lon)
+def decode(coord):
+    #Converts DDDMM.MMMMM > DD deg MM.MMMMM min
+    x = coord.split(".")
+    head = x[0]
+    tail = int(x[1])
+    deg = int(head[0:-2])
+    min = int(head[-2:])
+    return deg , "deg" , min , tail , "min"
 
 
-		if hasattr(parsed_data, "altitude"):
+def GPS_function(select):
 
-			if parsed_data.altitude != '':
-				gps_update["altitude"] = float(parsed_data.altitude)
+	if select:
+		mode = 1 
+	else:
+		mode = 0
 
+	NMEA_DICT = []
+	data = ''
+	
+	time = None
+	lat = None
+	dirLat = None
+	lon = None
+	dirLon = None
+	speed = None
+	trCourse = None
+	date = None	
+	sat_view = 0
+	sat_viewA = 0
+	sat_viewB = 0
+	pos_val = 'N'
+	alt = 0
+	lat_compat = 0
+	lon_compat = 0
+	
+	ser = serial.Serial(port, baud, timeout=3)
 
-		if hasattr(parsed_data, "speed"):
-
-			if parsed_data.speed != '':
-				gps_update["speed"] = float(parsed_data.speed)
-
-		return gps_update
-
+	# reading in serial stream
+	while True:
+		data_array = [0]
+		while ser.inWaiting() > 0:
+			if mode == 0:
+				data = ser.read(ser.inWaiting())
+			if mode == 1:
+				data = ser.readline(ser.inWaiting())
+			# converting bytes to string
+			data_stream = data.decode()
+			data = data_stream
+			# removing special symbols like linefeed , return 
+			data = data.replace('\n','')
+			data = data.replace('\t','')
+			data = data.replace('\r','')
+			# creating a array by coma split
+			data_array = data.split(',')
+			if  data_array[0] != '$GPTXT':
+				#adding the payload to may data to get the abbility to later query the data too in relation 
+				payload = NMEAReader.parse(data_stream,validate=0)
+				#print("payload:", payload )
+				# creating a matrix by adding the array in the NEMA array
+				data_array.append(payload)
+				NMEA_DICT.append(data_array)
+		#stopping the serial read when i detect the last sentence from a NEMA block 
+		if mode == 0:
+			if data_array[0] == '$GPGLL':
+				break
+		if mode == 1:
+			if data_array[0] == '$GNVTG':
+				break
+		
+	for item in NMEA_DICT:
+		#print("item", item )	
+		if "$GPRMC" == item[0] or "$GNRMC" == item[0]: 
+			if item[2] == 'V':
+				print("no satellite data available")
+				return 1, "no satellite data available"
+			#print("---Parsing GPRMC---")
+			time0, time1, time2 = int(item[1][0:2]), int(item[1][2:4]), int( item[1][4:6])
+			
+			lat0, lat1, lat2 ,lat3 ,lat4 = decode(item[3]) #latitude			
+			if hasattr(item[13], "lat"):
+				if item[13].lat != '':
+					lat_compat = float(item[13].lat)
+					
+			dirLat = item[4]      #latitude direction N/S
+			if hasattr(item[13], "NS"):
+				if item[13].NS != '':
+					dirLat = str(item[13].NS)			
+			
+			lon0, lon1, lon2, lon3, lon4 = decode(item[5]) #longitute		
+			if hasattr(item[13], "lon"):
+				if item[13].lon != '':
+					lon_compat = float(item[13].lon)	
+										
+			dirLon = item[6]      #longitude direction E/W	
+			if hasattr(item[13], "EW"):
+				if item[13].EW != '':
+					dirLon = str(item[13].EW)			
+			
+			# setting speed from own parsing and then try pynmeagps
+			speed = float(item[7])      #Speed in knots		
+			if hasattr(item[13], "spd"):
+				if item[13].lon != '':
+					speed = float(item[13].spd)		
+			
+			if item[8] == '':
+				trCourse = 0
+			else:
+				trCourse = float(item[8])   #True course
+				if hasattr(item[13], "cog"):
+					if item[13].cog != '':
+						speed = float(item[13].cog)					
+			
+			date0, date1, date2 = int(item[9][0:2]), int(item[9][2:4]), int(item[9][4:6]) #date
+			# thanks to this short display of the date do i have to fix it when i m dead
+			epoch = datetime.datetime(date2+2000, date1, date0, time0, time1, time2).timestamp() 
+		if '$GPGSV' == item[0]:
+			sat_viewA = int(item[1])
+		if '$GPGLL' == item[0]:
+			pos_val = item[6]
+			if hasattr(item[8], "status"):
+				if item[8].status != '':
+					pos_val = str(item[8].status)	
+			
+		if '$GPGGA' == item[0]:
+			sat_viewB = int(item[7])
+			if hasattr(item[15], "numSV"):
+				if item[15].numSV != '':
+					sat_viewB = float(item[15].numSV)	
+			
+			if item[9] != '':
+				alt = float(item[9])				
+				if hasattr(item[15], "alt"):
+					if item[15].alt != '':
+						alt = float(item[15].alt)	
+				
+			
+	if sat_viewB == 0:
+		sat_view = sat_viewA
+	else:
+		sat_view = sat_viewB
+		
+	
+		
+	gps_update = {"lat" : lat_compat, "lon" : lon_compat, "speed" : speed, "altitude" : alt, "track" : trCourse, "sats" : sat_view , "lat0" : lat0 , "lat1" : lat1, "lat2" : lat2 ,"lat3" : lat3 ,"lat4" : lat4 , "dirLat" : dirLat, "lon0" : lon0 , "lon1" : lon1, "lon2" : lon2, "lon3" : lon3, "lon4" : lon4, "dirLon" : dirLon,"pos_val" : pos_val, "time" : epoch}
+				
+	return gps_update
 
 class sensor(object):
 
@@ -360,18 +478,40 @@ class sensor(object):
 		global local_gps
 		# we get it anytime because its different per sensor read
 		timestamp = time.time()
-		position = [None,None,timestamp, configure.rabbitmq_tag]
+		position = [None, None, 0, 0, 0,0 , None , None, None, None ,None , None, None , None , None , None, None, None , None, timestamp, configure.rabbitmq_tag]
 		# braching of local gps information , i want to attach local gps information to the sensor data, so that i can send them with the readings and wenn i combine the 2 devices, can i see wehre the data is from and dont have to puzzle data together later 
 		if configure.gps:
 			# when gps is on , try to read the data here and fill the variables to send it
-			gps_data = GPS_function()
-			position = [gps_data["lat"],gps_data["lon"],timestamp, configure.rabbitmq_tag]
+			gps_data = GPS_function(False)
+			position = [
+			gps_data["lat"],
+			gps_data["lon"],
+			gps_data["speed"],
+			gps_data["altitude"],
+			gps_data["track"],
+			gps_data["sats"],
+			gps_data["lat0"],
+			gps_data["lat1"],
+			gps_data["lat2"],
+			gps_data["lat3"],
+			gps_data["lat4"],
+			gps_data["dirLat"],
+			gps_data["lon0"],
+			gps_data["lon1"],
+			gps_data["lon2"],
+			gps_data["lon3"],
+			gps_data["lon4"],
+			gps_data["dirLon"],
+			gps_data["pos_val"],
+			gps_data["time"],
+			configure.rabbitmq_tag
+			]
 		
 		# this part stores gps data in a global to fill sensor data , when we dont have data do we fill a location thats known to be wrong but nice to know 
 		if position[0] is not None and position[1] is not None :
 			local_gps = position
 		else:
-			local_gps = [37.7820885,-122.3045112,configure.rabbitmq_tag]
+			local_gps = [37.7820885,-122.3045112, 0, 0, 0,0 , 37 , 'deg', 78, 20885 , 'min' , 'N', 122 , 'deg' ,30 , 45112, 'min', 'W' , 'N', timestamp, configure.rabbitmq_tag]
 		
 		# any case retun, if its not none gets send
 		return position 
