e44a68b207c623335b4cf2a1c9f56a743c41d69c
4PXKHF9IK8ZL7XKN <95d4938ovw6vh9mw62ik@mailbox.org>
2024-08-21 11:35:42 +0200

    added the NEMA Reader to get a more preciese location calculatet from NEMA and found also a option that he should not jump over lines when he had checksum errors , so i pare the data to and save over my values when i can find them

--- test_programms/gps.py
+++ test_programms/gps.py
@@ -5,6 +5,7 @@
 import serial
 import time
 import datetime
+from pynmeagps import NMEAReader
 
 from serial import Serial
 
@@ -21,7 +22,7 @@ def GPS_function(select):
 	else:
 		mode = 0
 
-	NEMA_DICT = []
+	NMEA_DICT = []
 	data = ''
 	
 	time = None
@@ -51,16 +52,21 @@ def GPS_function(select):
 			if mode == 1:
 				data = ser.readline(ser.inWaiting())
 			# converting bytes to string
-			data = data.decode()
+			data_stream = data.decode()
+			data = data_stream
 			# removing special symbols like linefeed , return 
 			data = data.replace('\n','')
 			data = data.replace('\t','')
 			data = data.replace('\r','')
 			# creating a array by coma split
 			data_array = data.split(',')
-			#print("Array:", data_array )
-			# creating a matrix by adding the array in the NEMA array
-			NEMA_DICT.append(data_array)
+			if  data_array[0] != '$GPTXT':
+				#adding the payload to may data to get the abbility to later query the data too in relation 
+				payload = NMEAReader.parse(data_stream,validate=0)
+				#print("payload:", payload )
+				# creating a matrix by adding the array in the NEMA array
+				data_array.append(payload)
+				NMEA_DICT.append(data_array)
 		#stopping the serial read when i detect the last sentence from a NEMA block 
 		if mode == 0:
 			if data_array[0] == '$GPGLL':
@@ -69,30 +75,48 @@ def GPS_function(select):
 			if data_array[0] == '$GNVTG':
 				break
 		
-	for item in NEMA_DICT:
-		#print("item", item )
+	for item in NMEA_DICT:
+		#print("item", item )	
 		if "$GPRMC" == item[0] or "$GNRMC" == item[0]: 
 			if item[2] == 'V':
 				print("no satellite data available")
 				return 1, "no satellite data available"
 			#print("---Parsing GPRMC---")
 			time0, time1, time2 = int(item[1][0:2]), int(item[1][2:4]), int( item[1][4:6])
-			lat0, lat1, lat2 ,lat3 ,lat4 = decode(item[3]) #latitude
-			lat_compat = round(float(item[3])*0.01,10) # moving dot super magic
+			
+			lat0, lat1, lat2 ,lat3 ,lat4 = decode(item[3]) #latitude			
+			if hasattr(item[13], "lat"):
+				if item[13].lat != '':
+					lat_compat = float(item[13].lat)
+					
 			dirLat = item[4]      #latitude direction N/S
-			if dirLat == 'S':
-				lat_compat = lat_compat *-1
-			lon0, lon1, lon2, lon3, lon4 = decode(item[5]) #longitute
-			lon_compat = round(float(item[5])*0.01,10) # moving dot super magic
-			dirLon = item[6]      #longitude direction E/W
-			if dirLon == 'W':
-				lon_compat = lon_compat *-1
-			speed = float(item[7])      #Speed in knots
+			if hasattr(item[13], "NS"):
+				if item[13].NS != '':
+					dirLat = str(item[13].NS)			
+			
+			lon0, lon1, lon2, lon3, lon4 = decode(item[5]) #longitute		
+			if hasattr(item[13], "lon"):
+				if item[13].lon != '':
+					lon_compat = float(item[13].lon)	
+										
+			dirLon = item[6]      #longitude direction E/W	
+			if hasattr(item[13], "EW"):
+				if item[13].EW != '':
+					dirLon = str(item[13].EW)			
+			
+			# setting speed from own parsing and then try pynmeagps
+			speed = float(item[7])      #Speed in knots		
+			if hasattr(item[13], "spd"):
+				if item[13].lon != '':
+					speed = float(item[13].spd)		
 			
 			if item[8] == '':
 				trCourse = 0
 			else:
-				trCourse = float(item[8])   #True course			
+				trCourse = float(item[8])   #True course
+				if hasattr(item[13], "cog"):
+					if item[13].cog != '':
+						speed = float(item[13].cog)					
 			
 			date0, date1, date2 = int(item[9][0:2]), int(item[9][2:4]), int(item[9][4:6]) #date
 			# thanks to this short display of the date do i have to fix it when i m dead
@@ -101,15 +125,29 @@ def GPS_function(select):
 			sat_viewA = int(item[1])
 		if '$GPGLL' == item[0]:
 			pos_val = item[6]
+			if hasattr(item[8], "status"):
+				if item[8].status != '':
+					pos_val = str(item[8].status)	
+			
 		if '$GPGGA' == item[0]:
 			sat_viewB = int(item[7])
+			if hasattr(item[15], "numSV"):
+				if item[15].numSV != '':
+					sat_viewB = float(item[15].numSV)	
+			
 			if item[9] != '':
-				alt = float(item[9])
+				alt = float(item[9])				
+				if hasattr(item[15], "alt"):
+					if item[15].alt != '':
+						alt = float(item[15].alt)	
+				
 			
 	if sat_viewB == 0:
 		sat_view = sat_viewA
 	else:
 		sat_view = sat_viewB
+		
+	
 		
 	gps_update = {"lat" : lat_compat, "lon" : lon_compat, "speed" : speed, "altitude" : alt, "track" : trCourse, "sats" : sat_view , "lat0" : lat0 , "lat1" : lat1, "lat2" : lat2 ,"lat3" : lat3 ,"lat4" : lat4 , "dirLat" : dirLat, "lon0" : lon0 , "lon1" : lon1, "lon2" : lon2, "lon3" : lon3, "lon4" : lon4, "dirLon" : dirLon,"pos_val" : pos_val, "time" : epoch}
 				
