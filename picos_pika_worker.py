216b7d4d3fa7f63a3323e1ace3525ecc3dc336cf
4PXKHF9IK8ZL7XKN <95d4938ovw6vh9mw62ik@mailbox.org>
2024-08-24 21:58:04 +0200

    the pika process drives me a bit crazy, i cant determ when the connection breaks , a new declare dindt solfe this, i may have to anncounce sig term to sensors rabbitmq and restart

--- picos_pika_worker.py
+++ picos_pika_worker.py
@@ -15,14 +15,14 @@ if configure.rabbitmq_remote:
 else:
 	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 
+channel = connection.channel()
+
 def declare_channel(channel):
 	# Setup Channels for Sensors
 	channel.exchange_declare(exchange='sensor_data', exchange_type='topic')
-	channel.queue_declare(queue='sensor_metadata')
-	# cleanup entreys , we could have multible index
-	channel.queue_purge(queue='sensor_metadata')
 
 def publish_worker(IN_routing_key,data):
+	global channel
 	stack = 'sensor_data'
 	if IN_routing_key == 'sensor_metadata':
 		stack = ''
@@ -32,23 +32,28 @@ def publish_worker(IN_routing_key,data):
 	time_unix = time.time()
 	try:
 		if message is not None:
-			channel = connection.channel()
 			declare_channel(channel)
 			channel.basic_publish(exchange=stack, routing_key=routing_key, body=message)
 		else:
 			print("Is that a buffer underflow?")
+		
 	except Exception as e:
-		print("An error occurred:",e)
+		print("An error occurred in picos Worker:",e)
 		try:
-			raise Exception('Terminating')
-		finally:
-			os._exit(1)
+			os._exit(os.EX_OK)
+		except:
+			pass
+
 	if DEBUG:
 		print(f" {time_unix} [x] Sent {stack} {routing_key}:{message}")
+	return
 
 def disconnect():
     connection.close()
 
 def main_pika_worker(var,var2):
+	print("Worker Hello")
+	if os.fork() != 0:  # <--
+		return 0
 	publish_worker(var,var2)
-	return
+	return 0
